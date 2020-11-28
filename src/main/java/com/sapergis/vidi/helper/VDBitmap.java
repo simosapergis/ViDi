ea0104ebbde58048a5c77a532c379eab3ae29036
Sapergis <sim.apergis@gmail.com>
2020-11-28 13:21:57 +0200

    Fixing autocapture flow when changing orientations

--- src/main/java/com/sapergis/vidi/helper/VDBitmap.java
+++ src/main/java/com/sapergis/vidi/helper/VDBitmap.java
@@ -6,9 +6,6 @@ import android.graphics.Matrix;
 public class VDBitmap  {
     private Bitmap bitmapImage = null;
     private int degreesToRotate;
-    private float width;
-    private float height;
-    private static final float ANGLE = 90.0f;
 
     public Bitmap getBitmapImage() {
         return bitmapImage;
@@ -26,26 +23,9 @@ public class VDBitmap  {
         this.degreesToRotate = degreesToRotate;
     }
 
-    public float getWidth() {
-        return width;
-    }
-
-    public void setWidth(float width) {
-        this.width = width;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    public void setHeight(float height) {
-        this.height = height;
-    }
 
     public Bitmap rotateBitmap(){
         Matrix matrix = new Matrix();
-        // 0 -> 90
-        //90 - > 0;
         matrix.postRotate(degreesToRotate);
         return Bitmap.createBitmap(bitmapImage, 0, 0, bitmapImage.getWidth(),
                 bitmapImage.getHeight(), matrix, true);
